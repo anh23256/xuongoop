d67ccb2a38472c84b99fa635d04e9b2bf1c0de91
Tống Văn Đức <tongduc315@gmail.com>
2024-06-03 21:19:01 +0700

    Ghép theme

--- src/Views/Admin/layouts/master.blade.php
+++ src/Views/Admin/layouts/master.blade.php
@@ -1,28 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zxx">
+
+<!-- Mirrored from demo.dashboardpack.com/sales-html/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 May 2024 07:23:13 GMT -->
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>@yield('title')</title>
 
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    @include('layouts.partials.head')
 </head>
 
-<body>
-    <div class="container">
-        <h1>@yield('title')</h1>
+<body class="crm_body_bg">
 
-        <div class="row">
-            @yield('content')
+
+    @include('layouts.partials.nav')
+
+    <section class="main_content dashboard_part large_header_bg">
+
+        @include('layouts.partials.topbar')
+
+        <div class="main_content_iner overly_inner ">
+            <div class="container-fluid p-0 ">
+
+                @yield('content')
+
+            </div>
         </div>
+
+        @include('layouts.partials.footer')
+    </section>
+
+    <div id="back-top" style="display: none;">
+        <a title="Go to Top" href="#">
+            <i class="ti-angle-up"></i>
+        </a>
     </div>
 
+    @include('layouts.partials.script')
+
+    @yield('script')
 </body>
 
 </html>
